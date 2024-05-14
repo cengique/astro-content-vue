ada549d7aa497cfae17cee3de54844d7110d14f8
Cengiz Gunay <cengique@users.sf.net>
2024-05-13 21:42:15 -0400

    added vue component with image from content collection

---
+++ src/components/vue.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { getCollection } from 'astro:content';
+const blogEntries = await getCollection('posts');
+</script>
+
+<template>
+<ul>
+    <li v-for="blog in blogEntries">
+      <a :href="`/my-blog-url/${blog.slug}`">{{blog.data.title}}</a>
+      <img :src="blog.data.image.src">
+    </li>
+</ul>
+</template>
